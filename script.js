68b16766336c8a2e01e9f94f3637c23b35cdca3c
Zydrunask <kniukstaz@gmail.com>
2024-09-03 17:32:16 +0300

    basic map

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
+const gameDOM = document.querySelector('.game');
+
+gameDOM.innerHTML = `<div class="line">${'<div class="sqr"></div>'.repeat(15)}
+</div>`.repeat(15) + '<div class="blockMan"></div>';
+
+const lineDOM = document.querySelector('.line');
+const sqrDOM = document.querySelector('.sqr');
 const blockManDOM = document.querySelector('.blockMan');
 
+const sqrSize = 32;
 
-let blockManAxisPos = {
-    x: 3,
-    y: 3,
-}
-window.addEventListener('keyup', event => {
-    if(event.key === 'w') {
-        blockManAxisPos.y--;
+const blockManPos = {
+    x: 0,
+    y: 0,
+};
+
+window.addEventListener('keydown', event => {
+    if (event.key === 'w') {
+            blockManPos.y--
     }
-    if(event.key === 's') {
-        blockManAxisPos.y++;
+    if (event.key === 's') {
+            blockManPos.y++
     }
-    if(event.key === 'd') {
-        blockManAxisPos.x++;
+    if (event.key === 'a') {
+            blockManPos.x--
     }
-    if(event.key === 'a') {
-        blockManAxisPos.x--;
+    if (event.key === 'd') {
+            blockManPos.x++
     }
-    blockManDOM.style.top = (40 * blockManAxisPos.y) + 'px';
-    blockManDOM.style.left = (40 * blockManAxisPos.x) + 'px';
-});
+
+    blockManDOM.style.top = (sqrSize * blockManPos.y) + 'px';
+    blockManDOM.style.left = (sqrSize * blockManPos.x) + 17 + 'px';
+    console.log(blockManPos.x, blockManPos.y);
+    
+    
+})
 
