21c94a0c9105dac6c9faacabcd543241600c0a4a
Zydrunask <kniukstaz@gmail.com>
2024-09-03 17:46:32 +0300

    up

--- script.js
+++ script.js
@@ -7,29 +7,37 @@ const lineDOM = document.querySelector('.line');
 const sqrDOM = document.querySelector('.sqr');
 const blockManDOM = document.querySelector('.blockMan');
 
-const sqrSize = 32;
+const sqrSize = 48;
 
 const blockManPos = {
-    x: 0,
-    y: 0,
+    x: -14,
+    y: -6,
 };
 
 window.addEventListener('keydown', event => {
     if (event.key === 'w') {
+        if (blockManPos.y > -7) {
             blockManPos.y--
+        }
     }
     if (event.key === 's') {
+        if (blockManPos.y < 7) {
             blockManPos.y++
+        }
     }
     if (event.key === 'a') {
+        if (blockManPos.x > -15) {
             blockManPos.x--
+        }
     }
     if (event.key === 'd') {
+        if (blockManPos.x < -1) {
             blockManPos.x++
+        }
     }
 
     blockManDOM.style.top = (sqrSize * blockManPos.y) + 'px';
-    blockManDOM.style.left = (sqrSize * blockManPos.x) + 17 + 'px';
+    blockManDOM.style.left = (sqrSize * blockManPos.x) + 'px';
     console.log(blockManPos.x, blockManPos.y);
     
     
