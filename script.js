aa9118e491974dad107df9c69d70d959d1b4f62d
Zydrunask <kniukstaz@gmail.com>
2024-09-04 13:31:39 +0300

    up

--- script.js
+++ script.js
@@ -1,46 +1,50 @@
 const gameDOM = document.querySelector('.game');
 const playBtnDOM = document.querySelector('.playBtn')
 
-// playBtnDOM.addEventListener('click', () => {
-    gameDOM.innerHTML = `<div class="line">${'<div class="sqr"></div>'.repeat(15)}
-    </div>`.repeat(15) + '<div class="blockMan"></div>';   
-// })
-
-const lineDOM = document.querySelector('.line');
-const sqrDOM = document.querySelector('.sqr');
-const blockManDOM = document.querySelector('.blockMan');
-
 const sqrSize = 48;
-
+    
 const blockManPos = {
     x: -14,
     y: -6,
 };
 
-window.addEventListener('keydown', event => {
-    if (event.key === 'w') {
-        if (blockManPos.y > -7) {
-            blockManPos.y--
-        }
-    }
-    if (event.key === 's') {
-        if (blockManPos.y < 7) {
-            blockManPos.y++
-        }
-    }
-    if (event.key === 'a') {
-        if (blockManPos.x > -15) {
-            blockManPos.x--
-        }
-    }
-    if (event.key === 'd') {
-        if (blockManPos.x < -1) {
-            blockManPos.x++
-        }
-    }
+playBtnDOM.addEventListener('click', () => {
+    gameDOM.innerHTML = `<div class="line">${'<div class="sqr"></div>'.repeat(15)}
+    </div>`.repeat(15) + '<div class="blockMan"></div>';
+    gameDOM.style.left = '24px';   
+
+    const lineDOM = document.querySelector('.line');
+    const sqrDOM = document.querySelector('.sqr');
+    const blockManDOM = document.querySelector('.blockMan');
     
-    blockManDOM.style.top = (sqrSize * blockManPos.y) + 'px';
-    blockManDOM.style.left = (sqrSize * blockManPos.x) + 'px';
-    console.log(blockManPos.x, blockManPos.y);
+
+    window.addEventListener('keydown', event => {
+        if (event.key === 'w') {
+            if (blockManPos.y > -7) {
+                blockManPos.y--
+            }
+        }
+        if (event.key === 's') {
+            if (blockManPos.y < 7) {
+                blockManPos.y++
+            }
+        }
+        if (event.key === 'a') {
+            if (blockManPos.x > -15) {
+                blockManPos.x--
+            }
+        }
+        if (event.key === 'd') {
+            if (blockManPos.x < -1) {
+                blockManPos.x++
+            }
+        }
+        
+        blockManDOM.style.top = (sqrSize * blockManPos.y) + 'px';
+        blockManDOM.style.left = (sqrSize * blockManPos.x) + 'px';
+        console.log(blockManPos.x, blockManPos.y);
+    })
 })
+
+
 
